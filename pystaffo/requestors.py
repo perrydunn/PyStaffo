dbdcc1dfcf46ab21f3fbcd32c27199d796e6e153
Peregrine Dunn <perrydunn@hotmail.co.uk>
2018-07-30 14:07:35 +0100

    json.loads explicit decoding

--- pystaffo/requestors.py
+++ pystaffo/requestors.py
@@ -9,14 +9,14 @@ def get(auth=None, url=None, extras=None):
     if extras:
         params.update(extras)
     r = requests.get(url=url, auth=auth, params=params)
-    data = json.loads(r.content)
+    data = json.loads(r.content.decode('utf-8'))
     try:
         pages = int(r.headers['Pages'])
         if pages > 1:
             for page in range(2, pages + 1):
                 params.update({'page': page})
                 r = requests.get(url=url, auth=auth, params=params)
-                data += json.loads(r.content)
+                data += json.loads(r.content.decode('utf-8'))
     except KeyError:
         pass
     return data
