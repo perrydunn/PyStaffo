e5a17a715ffa0b54cc5e89c0a19ed8cb8479684e
Peregrine Dunn <perrydunn@hotmail.co.uk>
2019-06-13 20:45:44 +0200

    MANIFEST to incl requirements.txt

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as fh:
 
 setup(
     name='PyStaffo',
-    version='0.2.0',
+    version='0.1.3',
     author='Peregrine Dunn',
     author_email='perrydunn@hotmail.co.uk',
     description='A Staffomatic API Python wrapper',
